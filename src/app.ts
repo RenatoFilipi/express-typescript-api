52dd85e81333ff442c06646786f01890e0139e27
RenatoFilipi <re.rosa98@gmail.com>
2021-08-26 16:00:09 -0300

    add build and start scripts, env files

--- src/app.ts
+++ src/app.ts
@@ -3,4 +3,8 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
+app.get("/", (request, response) => {
+  return response.json({ msg: "Hello Developers!" });
+});
+
 export { app };
